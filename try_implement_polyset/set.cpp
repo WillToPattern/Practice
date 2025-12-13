ecbc9d47f5c65514cf82a4e950a4ca66c6c27948
Ayoub <107939144+WillToPattern@users.noreply.github.com>
2025-12-14 00:19:18 +0100

    Implement insert, print, clear, and has methods

--- try_implement_polyset/set.cpp
+++ try_implement_polyset/set.cpp
@@ -1,2 +1,27 @@
 #include "set.hpp"
 
+void set::insert(int val) {
+    if (!this->bag.has(val))
+        this->bag.insert(val);
+}
+
+void set::insert(int *array, int size) {
+    for (int i = 0; i < size; i++)
+        this->insert(array[i]);
+}
+
+void set::print() const {
+    this->bag.print();
+}
+
+void set::clear() {
+    this->bag.clear();
+}
+
+bool set::has(int val) const {
+    return this->bag.has(val);
+}
+
+searchable_bag &set::get_bag() const {
+    return this->bag;
+}
