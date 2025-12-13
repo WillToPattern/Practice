a9db7b8c6211dba9a912218c3e1c8f810fd8d10b
Ayoub <107939144+WillToPattern@users.noreply.github.com>
2025-12-14 00:23:06 +0100

    Implement has function to check for value presence

--- try_implement_polyset/searchable_array_bag.cpp
+++ try_implement_polyset/searchable_array_bag.cpp
@@ -1,1 +1,11 @@
 #include "searchable_array_bag.hpp"
+
+bool searchable_array_bag::has(int val) const
+{
+    for (int i = 0; i < this->size; i++)
+    {
+        if (this->data[i] == val)
+            return true;
+    }
+    return false;
+}
