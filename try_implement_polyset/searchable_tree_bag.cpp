502fc72fb7af083ca539e9a3e1398825133f1a81
Ayoub <107939144+WillToPattern@users.noreply.github.com>
2025-12-14 00:21:12 +0100

    Implement search functionality in searchable_tree_bag

--- try_implement_polyset/searchable_tree_bag.cpp
+++ try_implement_polyset/searchable_tree_bag.cpp
@@ -1,1 +1,19 @@
 #include "searchable_tree_bag.hpp"
+
+bool searchable_tree_bag::has(int val) const
+{
+    return (search_node(this->tree, val));
+}
+
+bool searchable_tree_bag::search_node(node* n, int val) const
+{
+    if (!n)
+        return false;
+    if (val == n->value)
+        return true;
+    if (val < n->value)
+        return search_node(n->l, val);
+    return search_node(n->r, val);
+
+
+}
