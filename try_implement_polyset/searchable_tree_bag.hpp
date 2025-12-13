8725c498e2d96b8ab7344d643f6385960587df78
Ayoub <107939144+WillToPattern@users.noreply.github.com>
2025-12-14 00:20:12 +0100

    Refactor searchable_tree_bag class structure
    Refactor searchable_tree_bag class to improve structure and add a virtual destructor. Added private search_node method for better encapsulation.

--- try_implement_polyset/searchable_tree_bag.hpp
+++ try_implement_polyset/searchable_tree_bag.hpp
@@ -1,26 +1,25 @@
-#include "searchable_bag.hpp"
-#include "tree_bag.hpp"
+#ifndef SEARCHABLE_TREE_BAG_HPP
+#define SEARCHABLE_TREE_BAG_HPP
 
-class searchable_tree_bag : public searchable_bag, public tree_bag{
-    public:
-        searchable_tree_bag() : tree_bag(){}
-        searchable_tree_bag(searchable_tree_bag& other) : tree_bag(other){}
-        searchable_tree_bag& operator=(const searchable_tree_bag& other){
-            if(this != &other){
-                tree_bag::operator=(other);
-            }  
-            return *this;
-        }
-        bool has(int item){
-            node* curr = tree;
-            while(curr){
-                if (item == curr->value)
-                    return true;
-                else if(item > curr->value)
-                    curr = curr->r;
-                else  
-                    curr = curr->l;
-            }
-            return false;
-        }
+#include "tree_bag.hpp"
+#include "searchable_bag.hpp"
+
+class searchable_tree_bag : public tree_bag, public searchable_bag {
+public:
+    searchable_tree_bag() : tree_bag() {};
+    searchable_tree_bag(const searchable_tree_bag &other) : tree_bag(other) {};
+    searchable_tree_bag &operator=(const searchable_tree_bag &other)
+    {
+        if (this != &other)
+            tree_bag::operator=(other);
+        return *this;
+    }
+    ~searchable_tree_bag() {};  // Make destructor virtual
+
+    bool has(int val) const;  // override for clarity (optional)
+
+private:
+    bool search_node(node* n, int val) const;
 };
+
+#endif
